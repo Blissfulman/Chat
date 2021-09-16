7f1a60261bac6a0eabdf6bf8cd217371269e45d4
Evgeny Novgorodov <greatly01@ya.ru>
2021-09-17 00:31:15 +0500

    Empty project was created

---
+++ Chat/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Chat
+//
+//  Created by Evgeny Novgorodov on 16.09.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
---
+++ Chat/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Evgeny Novgorodov on 16.09.2021.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
