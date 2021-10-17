7a766612dc87bd9dbea514640ccd6c8003dabab9
Evgeny Novgorodov <greatly01@ya.ru>
2021-10-17 23:01:44 +0500

    ProfileViewController was updated to handle an appearing of the keyboard

--- Chat/Presentation/ViewControllers/Profile/ProfileViewController.swift
+++ Chat/Presentation/ViewControllers/Profile/ProfileViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Evgeny Novgorodov on 26.09.2021.
 //
 
-final class ProfileViewController: UIViewController {
+final class ProfileViewController: KeyboardNotificationsViewController {
     
     // MARK: - Private properties
     
@@ -50,18 +50,18 @@ final class ProfileViewController: UIViewController {
         return imageView
     }()
     
-    private var fullNameLabel: UILabel = {
-        let label = UILabel().prepareForAutoLayout()
-        label.font = Fonts.subTitle
-        return label
+    private var fullTextField: UITextField = {
+        let textField = UITextField().prepareForAutoLayout()
+        textField.font = Fonts.subTitle
+        return textField
     }()
     
-    private var descriptionLabel: UILabel = {
-        let label = UILabel().prepareForAutoLayout()
-        label.font = Fonts.body
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        return label
+    private var descriptionTextView: UITextView = {
+        let textView = UITextView().prepareForAutoLayout()
+        textView.font = Fonts.body
+        textView.isScrollEnabled = false
+        textView.textAlignment = .center
+        return textView
     }()
     
     private var editAvatarButton: UIButton = {
@@ -70,6 +70,24 @@ final class ProfileViewController: UIViewController {
         button.setTitleColor(Palette.buttonTitleBlue, for: .normal)
         button.setTitle("Edit", for: .normal)
         button.addTarget(self, action: #selector(editAvatarButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private var buttonsStackView: UIStackView = {
+        let stackView = UIStackView().prepareForAutoLayout()
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    private var cancelButton: UIButton = {
+        let button = UIButton().prepareForAutoLayout()
+        button.backgroundColor = Palette.lightBarColor
+        button.titleLabel?.font = Fonts.buttonTitle
+        button.setTitleColor(Palette.buttonTitleBlue, for: .normal)
+        button.setTitle("Cancel", for: .normal)
+        button.setCornerRadius(14)
+        button.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
         return button
     }()
     
@@ -85,6 +103,8 @@ final class ProfileViewController: UIViewController {
     }()
     
     private let imagePickerController = UIImagePickerController()
+    private var buttonsStackViewBottomConstraint: NSLayoutConstraint?
+    private let defaultLowerButtonsBottomSpacing: CGFloat = 30
     
     // MARK: - Initialization
     
@@ -107,6 +127,26 @@ final class ProfileViewController: UIViewController {
     
     override func viewWillLayoutSubviews() {
         avatarImageView.round()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    // MARK: - KeyboardNotificationsViewController
+    
+    override func keyboardWillShow(_ notification: Notification) {
+        animateWithKeyboard(notification: notification) { keyboardFrame in
+            self.buttonsStackViewBottomConstraint?.constant = -keyboardFrame.height
+                - self.defaultLowerButtonsBottomSpacing
+        }
+    }
+    
+    override func keyboardWillHide(_ notification: Notification) {
+        animateWithKeyboard(notification: notification) { _ in
+            self.buttonsStackViewBottomConstraint?.constant = -self.defaultLowerButtonsBottomSpacing
+        }
     }
     
     // MARK: - Actions
@@ -145,6 +185,11 @@ final class ProfileViewController: UIViewController {
     }
     
     @objc
+    private func cancelButtonTapped() {
+        print("Cancel tapped")
+    }
+    
+    @objc
     private func saveButtonTapped() {
         print("Save tapped")
     }
@@ -154,14 +199,16 @@ final class ProfileViewController: UIViewController {
     private func setupUI() {
         view.roundCorners([.topLeft, .topRight], radius: 18)
         
-        view.addSubview(topView)
         view.addSubview(centralStackView)
         view.addSubview(editAvatarButton)
-        view.addSubview(saveButton)
+        view.addSubview(buttonsStackView)
         topView.addSubview(topStackView)
+        // Добавляется последним, чтобы быть более верхним слоем поверх остального контента
+        view.addSubview(topView)
         
         topStackView.addArrangedSubviews(titleLabel, closeButton)
-        centralStackView.addArrangedSubviews(avatarImageView, fullNameLabel, descriptionLabel)
+        centralStackView.addArrangedSubviews(avatarImageView, fullTextField, descriptionTextView)
+        buttonsStackView.addArrangedSubviews(cancelButton, saveButton)
     }
     
     private func setupLayout() {
@@ -169,17 +216,17 @@ final class ProfileViewController: UIViewController {
             topView.topAnchor.constraint(equalTo: view.topAnchor),
             topView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             topView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            topView.heightAnchor.constraint(equalToConstant: 96),
+            topView.heightAnchor.constraint(equalToConstant: 70),
             
             topStackView.topAnchor.constraint(equalTo: topView.topAnchor),
             topStackView.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: 16),
             topStackView.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -16),
             topStackView.bottomAnchor.constraint(equalTo: topView.bottomAnchor),
             
-            centralStackView.topAnchor.constraint(equalTo: topView.bottomAnchor, constant: 10),
+            centralStackView.topAnchor.constraint(lessThanOrEqualTo: topView.bottomAnchor, constant: 10),
             centralStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             centralStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            centralStackView.bottomAnchor.constraint(lessThanOrEqualTo: saveButton.topAnchor, constant: -16),
+            centralStackView.bottomAnchor.constraint(lessThanOrEqualTo: buttonsStackView.topAnchor, constant: -16),
             
             avatarImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
             avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
@@ -187,18 +234,25 @@ final class ProfileViewController: UIViewController {
             editAvatarButton.trailingAnchor.constraint(equalTo: avatarImageView.trailingAnchor),
             editAvatarButton.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor),
             
-            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant:  50),
-            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:  -50),
-            saveButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+            buttonsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant:  50),
+            buttonsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:  -50),
+            
+            cancelButton.heightAnchor.constraint(equalToConstant: 40),
             saveButton.heightAnchor.constraint(equalToConstant: 40)
         ])
+        
+        buttonsStackViewBottomConstraint = buttonsStackView.bottomAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+            constant: -30
+        )
+        buttonsStackViewBottomConstraint?.isActive = true
     }
     
     private func configureUI() {
         view.backgroundColor = .white
         imagePickerController.delegate = self
-        fullNameLabel.text = "Marina Dudarenko" // TEMP
-        descriptionLabel.text = "UX/UI designer, web-designer Moscow, Russia" // TEMP
+        fullTextField.text = "Marina Dudarenko" // TEMP
+        descriptionTextView.text = "UX/UI designer, web-designer Moscow, Russia" // TEMP
     }
 }
 
