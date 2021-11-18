59b7151ea12d2991d5e56f4be5689e5e5e6c3f8a
Evgeny Novgorodov <greatly01@ya.ru>
2021-11-18 16:51:46 +0500

    The documenting comments to SyncProfileDataManager were added

--- Chat/Core layer/Managers/Protocols/SyncProfileDataManager.swift
+++ Chat/Core layer/Managers/Protocols/SyncProfileDataManager.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
+/// Менеджер синхронного взаимодействия с хранилищем данных профиля.
 protocol SyncProfileDataManager {
+    /// Синхронное сохранение данных профиля.
+    /// - Parameter profile: Профиль.
     func saveProfile(profile: Profile) -> Result<Void, Error>
+    /// Синхронное получение данных профиля.
     func fetchProfile() -> Result<Profile?, Error>
 }
